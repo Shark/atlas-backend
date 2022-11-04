661a6ac7e4bbd7ef7f8901c346c061c237583962
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-11-05 00:13:28 +0200

    initial commit

---
+++ handler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// event contains data that is passed to the function
+
+module.exports.openaiRequest = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
