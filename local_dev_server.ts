a9f74a22c5bd5b4767814d2a82768a696630b4a7
Jonas Schiefner <jonas.schiefner@zweitag.de>
2022-11-05 19:18:37 +0200

    enable cors for local dev server

--- local_dev_server.ts
+++ local_dev_server.ts
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require("express");
 const handler = require("./handler")
+const cors = require('cors')
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 app.post('*', (req: any, res: any) => {
     const path = req.path.slice(1); // slices out first '/
