c061c1a09ba80e95ad929a4fe4af95c7a08eaf68
Felix Seidel <felix@seidel.me>
2022-11-13 18:39:48 +0200

    Add token auth

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM node:18-alpine
+FROM node:18-alpine AS builder
 RUN mkdir /app
-COPY package.json package-lock.json /app/
 WORKDIR /app
+COPY package.json package-lock.json ./
 RUN npm install
-COPY .build/ /app/
-CMD ["server.js"]
+COPY ./ ./
+RUN npm run compile
+
+FROM node:18-alpine AS runtime
+RUN mkdir /app
+WORKDIR /app
+COPY --from=builder /app/package.json /app/package-lock.json ./
+RUN npm install --omit=dev
+COPY --from=builder /app/.build/ ./
+CMD ["/app/server.js"]
--- server.ts
+++ server.ts
@@ -1,20 +1,48 @@
+import { timingSafeEqual } from 'crypto';
 import * as dotenv from 'dotenv'
 dotenv.config();
 import express from 'express'
 import handler from './index.js';
 import cors from 'cors';
 
+const port = process.env.PORT || 3000;
+const token: string = process.env.TOKEN || '';
+if(token.length === 0) {
+    throw new Error('TOKEN must be set');
+}
+
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
-app.post('*', (req: any, res: any) => {
+function ensureToken(req: any, res: any, next: any) {
+    var bearerHeader = req.headers["authorization"];
+    var message = 'Unauthorized';
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        if(bearer.length === 2) {
+            const requestToken = bearer[1]
+            if(timingSafeEqual(Buffer.from(requestToken, 'utf8'), Buffer.from(token, 'utf8'))) {
+                next();
+                return;
+            } else {
+                message = 'Token Mismatch';
+            }
+        } else {
+            message = 'Header Malformed'
+        }
+    }
+    res.status(403);
+    res.send(message);
+}
+
+app.post('*', ensureToken, (req: any, res: any) => {
     const path = req.path.slice(1); // slices out first '/'
 
     // call handler with body, context as null and response handler callback
     // @ts-ignore
     handler[path](req.body, null, (_: any, response: any) => {
-        const {statusCode, headers, body} = response;
+        const { statusCode, headers, body } = response;
 
         res.set(headers);
         res.status(statusCode)
@@ -22,6 +50,6 @@ app.post('*', (req: any, res: any) => {
     });
 });
 
-app.listen(8000, () => {
-    console.log('Server listening on port 8000');
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
